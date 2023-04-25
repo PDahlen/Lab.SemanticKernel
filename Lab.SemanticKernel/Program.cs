930c11c874b0ea1a6b1c2b60c2b53c72a4e87357
Patrik Dahlén <patrik_dahlen@hotmail.com>
2023-04-25 12:04:45 +0200

    Added user secrets

--- Lab.SemanticKernel/Program.cs
+++ Lab.SemanticKernel/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ReadSecretsConsole;
+
+var secretAppsettingReader = new SecretAppsettingReader();
+var openAISecretValues = secretAppsettingReader.ReadSection("OpenAI", "ApiKey");
+var azureOpenAISecretValues = secretAppsettingReader.ReadSection("AzureOpenAI", "ApiKey");
+
+Console.WriteLine($"The value for {openAISecretValues.Path} is: {openAISecretValues.Value}");
+Console.WriteLine($"The value for {openAISecretValues.Path} is: {azureOpenAISecretValues.Value}");
+
+Console.ReadKey();
---
+++ Lab.SemanticKernel/SecretAppsettingReader.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ReadSecretsConsole;
+
+public class SecretAppsettingReader
+{
+    public IConfigurationSection ReadSection(string sectionName, string key)
+    {
+        var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables();
+        var configurationRoot = builder.Build();
+
+        var section = configurationRoot.GetSection(sectionName).GetSection(key);
+
+        return section;
+    }
+}
